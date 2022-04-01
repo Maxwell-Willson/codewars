4f94d2ccee4e67925cd74bd21757059878ffc018
Maxwell-Willson <maxcwillson@gmail.com>
2022-04-01 10:37:53 -0400

    completed two code wars challenges

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// 1. Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+
+function greet(name){
+   return `Hello, ${name} how are you doing today?`
+  }
+
+
+// 2. 
+
+
+const Ball = function(ballType) { ballType === undefined ? this.ballType = "regular" : this.ballType = ballType
+
+   };

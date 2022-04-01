ec47182de6221372c4f92dc75ea206430f232409
Maxwell-Willson <maxcwillson@gmail.com>
2022-04-01 11:39:46 -0400

    completed 1 more codewars

--- js/main.js
+++ js/main.js
@@ -6,9 +6,26 @@ function greet(name){
   }
 
 
-// 2. 
+// 2. Create a class Ball. Ball objects should accept one argument for "ball type" when instantiated.
+
+// If no arguments are given, ball objects should instantiate with a "ball type" of "regular."
 
 
 const Ball = function(ballType) { ballType === undefined ? this.ballType = "regular" : this.ballType = ballType
 
    };
+
+//    Description:
+//    It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string.
+//     You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+
+
+function removeChar(str){
+let strArr = str.split('');
+strArr.pop();
+strArr.shift();
+return strArr.join('');
+   }
+   
+
+removeChar("testString")

212080e4b10ab6207990a7118ab40a70e57f0eed
Maxwell-Willson <maxcwillson@gmail.com>
2022-04-02 17:00:36 -0400

    added a completed kyu7

--- js/main.js
+++ js/main.js
@@ -74,3 +74,27 @@ function stringToArray(string){
    return string.split(' ')
       
    }
+
+
+   //Take 2 strings s1 and s2 including only letters from ato z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2
+
+
+   function longest(s1, s2) {
+      //turn both strings into array to analyze/execute methods on
+      let s1Arr = s1.split('')
+      let s2Arr = s2.split('')
+      //combine the two arrays to have one array of the entire list of optoins'
+      let combinedArr = s1Arr.concat(s2Arr)
+      //filter the new combined array, if the index of the item is different then the indexOf() value its a duplicate, so we filter those out, return this value
+      let finalAnswerArr = combinedArr.filter((c, index) => {
+         return combinedArr.indexOf(c) === index;
+      });
+      // sort the combined array, then turn it into a string to return
+      return finalAnswerArr.sort().join('');
+    }
+
+    let a = "xyaabbbccccdefww"
+
+    let b = "xxxxyyyyabklmopq"
+
+    longest(a, b)

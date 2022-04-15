d90a7057c4fd7bbd3588f31b363276456c3758e6
Maxwell-Willson <maxcwillson@gmail.com>
2022-04-15 10:47:49 -0400

    finished daily challenge

--- js/main.js
+++ js/main.js
@@ -617,3 +617,43 @@ function solutionSumMultiplesFiveOrThree(number){
 }
 
 // solutionSumMultiplesFiveOrThree(10)
+
+
+// 27. In this Kata we are passing a number (n) into a function.
+
+// Your code will determine if the number passed is even (or not).
+
+// The function needs to return either a true or false.
+
+// Numbers may be positive or negative, integers or floats.
+
+// Floats with decimal part non equal to zero are considered UNeven for this kata.
+
+
+function testEven(n) {
+  if (Math.floor(n) !== n){
+     return false
+  } else if(n % 2 === 0){
+     return true
+  }else{
+     return false
+  }
+}
+
+// 28.
+
+// Your coworker was supposed to write a simple helper function 
+// to capitalize a string (that contains a single word) before they went on vacation.
+
+// Unfortunately, they have now left and the code they gave you doesn't work. 
+// Fix the helper function they wrote so that it works as intended (i.e. make the first character in the string "word" upper case).
+
+// Don't worry about numbers, special characters, or non-string types being passed to the function.
+//  The string lengths will be from 1 character up to 10 characters, but will never be empty.
+
+
+ function capitalizeWord(word) {
+  let wordArray = word.split('')
+  wordArray[0] = wordArray[0].toUpperCase()
+  return wordArray.join('')
+ }

ad57840340b3fdd7de45b7ba4ca21f3eb94b80fd
Maxwell-Willson <maxcwillson@gmail.com>
2022-05-23 12:12:47 -0400

    two codewars

--- js/main.js
+++ js/main.js
@@ -1220,4 +1220,55 @@ function descendingOrder(n){
    return Number(concat)
  }
 
- descendingOrder(123456789)
+//  descendingOrder(123456789)
+
+
+
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+function booleanToString(b){
+   return b ? 'true' : 'false';
+ }
+// Don't give me five!
+// In this kata you get the start number and the end number of a region and should return the count of all numbers except numbers with a 5 in it.
+//  The start and the end number are both inclusive!
+
+// Examples:
+
+// 1,9 -> 1,2,3,4,6,7,8,9 -> Result 8
+// 4,17 -> 4,6,7,8,9,10,11,12,13,14,16,17 -> Result 12
+// The result may contain fives. ;-)
+// The start number will always be smaller than the end number. Both numbers can be also negative!
+
+// I'm very curious for your solutions and the way you solve it. Maybe someone of you will find an easy pure mathematics solution.
+
+// Have fun coding it and please don't forget to vote and rank this kata! :-)
+
+// I have also created other katas. Take a look if you enjoyed this kata!
+
+
+function dontGiveMeFive(start, end)
+{
+   fullArrOfN = []
+   dontGiveFiveArr = []
+  //populate an array with values from start to end
+  for (let i = start; i <= end; i++){
+   fullArrOfN.push(i)
+  }
+  //turn each individual item in array, into an array
+  let splitArray = fullArrOfN.map(item => (item.toString().split('')));
+  //look through each array and see if a '5' exists
+  for(let i = 0; i < splitArray.length; i++){
+     if(splitArray[i].includes('5') === false){
+      dontGiveFiveArr.push(splitArray[i])
+     }
+
+}
+return(dontGiveFiveArr.length)
+}
+
+// dontGiveMeFive(1,9)
+
+
